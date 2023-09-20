87918fa8aa8dab809e86c3edf866877f1f92f745
ArttuuS <arttu.saily@gmail.com>
2023-09-21 01:28:59 +0300

    toka muokkaus

--- src/main/java/hh/sof03/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/Bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package hh.sof03.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.Bookstore.domain.Book;
+import hh.sof03.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,18 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book b1 = new Book("Taru sormusten herrasta", "Kirjailija Kirja", 1998, "1281408565652", 21.90);
+			Book b2 = new Book("Esimerkkikirja2", "Risto Matti", 1923, "9781408565652", 89.90);
+			Book b3 = new Book("Mahtava kirja", "Leevi Leeviläinen", 2008, "7881408565652", 15.90);
+
+			repository.save(b1);
+			repository.save(b2);
+			repository.save(b3);
+
+		};
+
+	}
 }
